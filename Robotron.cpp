33af7fdb90503ffea694b62f702023b5e1ddec23
Geraint-Thomas <st20285303@outlook.cardiffmet.ac.uk>
2025-12-23 15:25:28 +0000

    changed if statments, removed brackets, removed loc function and small optimisations

--- Robotron.cpp
+++ Robotron.cpp
@@ -6,11 +6,6 @@
 using namespace std;
 
 int x = 0, y = 0; // Screen size position
-int loc(int _x, int _y)
-{
-	return _x + (_y * x); // Calculate the position in the screen buffer
-}
-
 int main()
 {
 	int r = 0, j = 0, l = 0, h=0; //number of robots, junk piles and lives
@@ -42,21 +37,19 @@ int main()
 
 	memset(screen, ' ', x * y); // Fill the screen with spaces
 
-	screen[loc(px, py)] = '@'; // Place player on the screen
+	screen[px+(py*x)] = '@'; // Place player on the screen
 
 	//place robots on the screen
 	for (int i = 0; i < r; i++)
 	{
 		int rx = rand() % x; // Random X position for robot
 		int ry = rand() % y; // Random Y position for robot
-		if (screen[loc(rx, ry)] == ' ')
-		{
-			screen[loc(rx, ry)] = '#'; // Place robot on the screen
-		}
+		if (screen[rx+(ry*x)] == ' ') 
+			screen[rx+(ry*x)] = '#'; // Place robot on the screen
+		
 		else
-		{
 			i--; // If position is occupied, retry placing the robot
-		}
+		
 	}
 
 	//place junk piles on the screen
@@ -64,14 +57,10 @@ int main()
 	{
 		int jx = rand() % x; // Random X position for junk
 		int jy = rand() % y; // Random Y position for junk
-		if (screen[loc(jx, jy)] == ' ')
-		{
-			screen[loc(jx, jy)] = '^'; // Place Junk on the screen
-		}
+		if (screen[jx+(jy*x)] == ' ')
+			screen[jx+(jy*x)] = '^'; // Place Junk on the screen
 		else
-		{
 			i--; // If position is occupied, retry placing the junk
-		}
 	}
 	// Game loop
 	while (l > 0 && r > 0) // Continue until lives or robots are exhausted
@@ -81,7 +70,7 @@ int main()
 		{
 			for (int j = 0; j < x; j++)
 			{
-				cout << screen[loc(j, i)];
+				cout << screen[j+(i*x)];
 			}
 			cout << endl;
 		}
@@ -98,12 +87,12 @@ int main()
 		case 'd': new_px++; break; // Move right
 		case 'h': // Hyperspace
 		{
-			if (h == 0) continue; // If hyperspace is not available, ignore input
-			while (screen[loc(new_px, new_py)] != ' ' && h>0) // Find a random empty space
-			{
-				new_px = rand() % x; // Random X position
+			if (!h) continue; // If hyperspace is not available, ignore input
+			while (screen[new_px + (new_py * x)] != ' ' && h > 0) // Find a random empty space
+
+				new_px = rand() % x, // Random X position
 				new_py = rand() % y; // Random Y position
-			};
+			;
 			h--; // Decrease hyperspace usage count
 			break;
 		}
@@ -112,30 +101,30 @@ int main()
 
 		if (new_px < 0 || new_px >= x || new_py < 0 || new_py >= y) continue; // Check bounds
 
-		screen[loc(px, py)] = ' '; // Clear old player position
+		screen[px+(py*x)] = ' '; // Clear old player position
 
-		if (screen[loc(new_px, new_py)] == '#') // If robot is encountered
-		{
-			l--; // Lose a life
-			screen[loc(new_px, new_py)] = '@'; // Clear robot
-			screen2[loc(new_px, new_py)] = '@'; // Update player position
-			px = new_px;
+		if (screen[new_px+(new_py*x)] == '#') // If robot is encountered
+		
+			l--, // Lose a life
+			screen[new_px+(new_py*x)] = '@', // Clear robot
+			screen2[new_px+(new_py*x)] = '@', // Update player position
+			px = new_px,
 			py = new_py;
-		}
+		
 		else // If junk or space is encountered
-		{
-			screen[loc(new_px, new_py)] = '@'; // Clear junk pile
-			screen2[loc(new_px, new_py)] = '@'; // Update player position
-			px = new_px;
+		
+			screen[new_px+(new_py*x)] = '@', // Clear junk pile
+			screen2[new_px+(new_py*x)] = '@', // Update player position
+			px = new_px,
 			py = new_py;
-		}
+		
 
 		//move robots and copy over junk piles
 		for (int i = 0; i < y; i++)
 		{
 			for (int j = 0; j < x; j++)
 			{
-				int lc = loc(j, i);
+				int lc = j+(i*x);
 
 				if (screen[lc] == '#') // If a robot is present
 				{
@@ -147,25 +136,21 @@ int main()
 					int new_rx = j + move_x;
 					int new_ry = i + move_y;
 
-					screen[loc(j, i)] = ' '; // Clear old robot position
+					screen[j+(i*x)] = ' '; // Clear old robot position
 
-					if (screen2[loc(new_rx, new_ry)] == '@') // If player is in the new position
-					{
+					if (screen2[new_rx+(new_ry*x)] == '@') // If player is in the new position
 						l--; // Lose a life
-					}
-					else if (screen[loc(new_rx, new_ry)] == '^' || screen2[loc(new_rx, new_ry)] == '#') // If junk or another robot is in the new position
-					{
-						screen2[loc(new_rx, new_ry)] = '^'; // Robot becomes junk
-					}
+					
+					else if (screen[new_rx+(new_ry*x)] == '^' || screen2[new_rx+(new_ry*x)] == '#') // If junk or another robot is in the new position
+						screen2[new_rx+(new_ry*x)] = '^'; // Robot becomes junk
+					
 					else
-					{
-						screen2[loc(new_rx, new_ry)] = '#'; // Move robot to new position
-					}
+						screen2[new_rx+(new_ry*x)] = '#'; // Move robot to new position
+					
 				}
 				else if (screen[lc] == '^') // If a junk pile is present
-				{
 					screen2[lc] = screen[lc];
-				}
+				
 			}
 		}
 
@@ -173,9 +158,8 @@ int main()
 		for (int i = 0; i < x * y; i++)
 		{
 			if (screen2[i] == '#') // Count remaining robots
-			{
 				r++;
-			}
+			
 		}
 
 		char* temp = screen; // Swap the screen buffers
@@ -183,11 +167,11 @@ int main()
 		screen2 = temp;
 
 	}
-	if (r == 0) // If all robots are destroyed
+	if (!r) // If all robots are destroyed
 	{
 		cout << "You win!" << endl;
 	}
-	else if (l == 0) // If player has no lives left
+	else if (!l) // If player has no lives left
 	{
 		cout << "You lose!" << endl;
 	}
